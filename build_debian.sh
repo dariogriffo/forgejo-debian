eb867a010dab41deb0817d2e112f51a7027c7ffe
Dario Griffo <dariogriffo@gmail.com>
2026-07-09 15:31:23 +0100

    Use ~ instead of + for the distro release suffix

--- build_debian.sh
+++ build_debian.sh
@@ -61,7 +61,7 @@ build_architecture() {
     declare -a arr=("bookworm" "trixie" "forky" "sid")
 
     for dist in "${arr[@]}"; do
-        FULL_VERSION="$FORGEJO_VERSION-${BUILD_VERSION}+${dist}_${build_arch}"
+        FULL_VERSION="$FORGEJO_VERSION-${BUILD_VERSION}~${dist}_${build_arch}"
         echo "  Building $FULL_VERSION"
 
         if ! docker build . -t "forgejo-$dist-$build_arch" \
--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -23,7 +23,7 @@ build_architecture() {
     declare -a arr=("jammy" "noble" "questing" "resolute")
 
     for dist in "${arr[@]}"; do
-        FULL_VERSION="$FORGEJO_VERSION-${BUILD_VERSION}+${dist}_${build_arch}_ubu"
+        FULL_VERSION="$FORGEJO_VERSION-${BUILD_VERSION}~${dist}_${build_arch}_ubu"
         echo "  Building $FULL_VERSION"
 
         if ! docker build . -f Dockerfile.ubu -t "forgejo-ubuntu-$dist-$build_arch" \
